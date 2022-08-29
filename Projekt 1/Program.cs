7fc7a28ddf6924d4bf2c056929e65666cb449c1e
lovisally <lovi05001@utb.vaxjo.se>
2022-08-29 10:38:22 +0200

    Uppgift 1-3
    Fastnade vid uppgift tre, funderade men kom ej fram till något.

---
+++ Projekt 1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+	Console.WriteLine("Är det fint väder?");
+		string svar = Console.ReadLine();
+		bool sant = svar == "ja"; 
+		
+		{
+			if(sant)
+			Console.WriteLine("gå ut!");
+			
+			else if(svar == "nej") 
+			Console.WriteLine("stanna inomhus!");
+
+            else(svar == "kanske")
+            Console.WriteLine("jag förstår inte.");
+	
+		}
